939fd627fcd84b4677fd644647b6c32ae0f40230
rizkiakbar2302@gmail.com <rizkiakbar2302@gmail.com>
2022-08-04 23:12:48 +0700

    tambah form input di tambah.php

---
+++ tambah.php
@@ -0,0 +1,29 @@
+<html>
+
+<head>
+    <title>
+        Tambah data Anggota
+    </title>
+</head>
+
+<body>
+    <h2>Silahkan Tambah data anggota</h2>
+    <br>
+    <form method="post" action="proses.php">
+        <label for="nama">Nama</label>
+        <input type="text" name="nama" id="nama">
+        <br>
+        <label for="no_telp">No Telp</label>
+        <input type="number" name="no_telp" id="no_telp">
+        <br>
+        <label for="alamat">Alamat</label>
+        <input type="text" name="alamat" id="alamat">
+        <br>
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email">
+        <br>
+        <input type="submit" value="SIMPAN">
+    </form>
+</body>
+
+</html>
