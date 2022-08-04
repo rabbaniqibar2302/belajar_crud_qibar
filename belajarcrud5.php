1b2c2303ab09eb14686fba2b3a84bf83bc181d3b
rizkiakbar2302@gmail.com <rizkiakbar2302@gmail.com>
2022-08-04 22:58:58 +0700

    tambah data table admin, anggota dan buku

--- belajarcrud5.php
+++ belajarcrud5.php
@@ -9,6 +9,7 @@
     <br />
     <br />
     <br />
+    <a href="tambah.php">Tambah Data</a>
     <h1>Anggota</h1>
     <table border="1">
         <tr>
@@ -31,6 +32,56 @@
                 <td><?php echo $d['no_tlp']; ?></td>
                 <td><?php echo $d['alamat']; ?></td>
                 <td><?php echo $d['email']; ?></td>
+            </tr>
+        <?php
+        }
+        ?>
+    </table>
+    <h1>Admin</h1>
+    <table border="1">
+        <tr>
+            <th>No</th>
+            <th>Id Admin</th>
+            <th>Nama</th>
+        </tr>
+
+        <?php
+        include 'koneksi.php';
+        $no = 1;
+        $data = mysqli_query($connect, "select * from admin");
+        while ($d = mysqli_fetch_array($data)) {
+        ?>
+            <tr>
+                <td><?php echo $no++; ?></td>
+                <td><?php echo $d['id_admin']; ?></td>
+                <td><?php echo $d['username']; ?></td>
+            </tr>
+        <?php
+        }
+        ?>
+    </table>
+    <h1>Buku</h1>
+    <table border="1">
+        <tr>
+            <th>No</th>
+            <th>Judul</th>
+            <th>Pengarang</th>
+            <th>Tahun Terbit</th>
+            <th>Penerbit</th>
+        </tr>
+
+        <?php
+        include 'koneksi.php';
+        $no = 1;
+        $data = mysqli_query($connect, "select * from buku");
+        while ($d = mysqli_fetch_array($data)) {
+        ?>
+            <tr>
+                <td><?php echo $no++; ?></td>
+                <td><?php echo $d['judul_buku']; ?></td>
+                <td><?php echo $d['pengarang']; ?></td>
+                <td><?php echo $d['thn_terbit']; ?></td>
+                <td><?php echo $d['penerbitan']; ?></td>
             </tr>
         <?php
         }
